6e08d488ee74c27f90fd9de0913718e3eabba5eb
wenxx666 <3501202786@qq.com>
2022-02-03 14:09:23 +0800

    test update

--- 220203-Fra.py
+++ 220203-Fra.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter.messagebox import *
 from fractions import Fraction
 from tkinter import font
+def b1Act():
+    asmd = Toplevel()
+    asmd.geometry('400x550')
+    asmd.title('分数四则运算')
 top = Tk()
 top.title('分数运算器')
 top.geometry('255x70')
@@ -9,7 +13,7 @@ top.resizable(False,False)
 f1 = font.Font(size=20)
 lab1 = Label(top,text='主菜单',font=f1)
 lab1.place(relx=0.5,rely=0.25,anchor=CENTER)
-butt1 = Button(top,text='四则运算')
+butt1 = Button(top,text='四则运算',command=b1Act)
 butt1.place(relx=0.2,rely=0.7,anchor=CENTER,relwidth=0.25)
 butt2 = Button(top,text='约分')
 butt2.place(relx=0.5,rely=0.7,anchor=CENTER,relwidth=0.25)
